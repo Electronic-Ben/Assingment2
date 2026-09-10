cb8e54c3a3b8ca6ba9b282628a3518af77a27ce0
Eben Higgins <ebenezer.higgins@gmail.com>
2026-09-10 15:25:13 -0700

    Add 'the plits' problem

---
+++ theSplits.py
@@ -0,0 +1,20 @@
+def get_int(msg, fail = "Invalid."):
+    try:
+        ans = int(input(msg))
+    except Exception as e:
+        print(fail)
+        return get_int(msg, fail)
+    return ans
+
+def is_5_digits(x):
+    return len(str(x))
+
+def get_5_digit_num():
+    x = get_int("Enter a 5 digit number: ")
+    if not is_5_digits(x):
+        print("Number must be 5 digits!")
+        return get_5_digit_num()
+    return x
+
+num = str(get_5_digit_num())
+print("   ".join(list(num)))
